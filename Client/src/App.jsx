b2b46d1cdc1b481a1e206b370072227d3145cbab
jeevanhd <jeevan.hd@kalvium.community>
2025-02-12 11:35:31 +0530

    m2

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello welcome to my app</h1>
+      <p>This is my first commit for Frontend</p>
+    </>
+  );
+}
+
+export default App;
