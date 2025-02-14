364336cbd57dd60ec997e2e02f4b8d821635c443
jeevanhd <jeevan.hd@kalvium.community>
2025-02-14 14:53:43 +0530

    m5

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -8,22 +8,21 @@ const getUserController = async (req, res) => {
       .status(200)
       .send({ message: "user data fetched successfully", data: data });
   } catch (error) {
-    return res
-      .status(500)
-      .send({
-        message: "internal server error",
-        success: false,
-        error: error.message,
-      });
+    return res.status(500).send({
+      message: "internal server error",
+      success: false,
+      error: error.message,
+    });
   }
 };
+
 const getSingleUserController = async (req, res) => {
   const { id } = req.query;
   try {
     const data = await UserModel.findOne({ _id: id });
 
     if (!data) {
-      return res.status(400).send({ message: "data not found" });
+      return res.status(400).send({ message: "user data not found" });
     }
 
     res.status(200).send({
@@ -38,4 +37,38 @@ const getSingleUserController = async (req, res) => {
   }
 };
 
-module.exports = { getUserController, getSingleUserController };
+const createUserController = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).send({ message: "all fields are required" });
+    }
+
+    const checkUserPresent = await UserModel.findOne({ email: email });
+    if (checkUserPresent) {
+      return res.status(400).send({ message: "user already present" });
+    }
+
+    const data = await UserModel.create({
+      name,
+      email,
+      password,
+    });
+
+    return res
+      .status(201)
+      .send({ message: "New user created successfully", success: true, data });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Internal server error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getUserController,
+  getSingleUserController,
+  createUserController,
+};
--- backend/src/router/user.router.js
+++ backend/src/router/user.router.js
@@ -2,10 +2,13 @@ const express = require("express");
 const {
   getUserController,
   getSingleUserController,
+  createUserController,
 } = require("../controller/user.controller");
 const router = express.Router();
 
 router.get("/get-user/", getUserController);
-router.get("get-single-user/", getSingleUserController);
+router.get("/get-single-user/", getSingleUserController);
+
+router.post("/create-user", createUserController);
 
 module.exports = router;
